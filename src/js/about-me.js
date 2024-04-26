7e763dd252b1132bb564c463a599b169bf552d61
vButkevich <vyacheslav.butkevich@gmail.com>
2024-04-26 19:22:51 +0300

    accordion

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -1,35 +1,19 @@
-// User options
-new Accordion('.container-second', {
-  duration: 400,
+import Accordion from 'accordion-js';
+//import 'accordion-js/dist/accordion.css';
+import '../css/accordion.css';
+
+const accordions = Array.from(
+  document.querySelectorAll('.accordion-container')
+);
+new Accordion(accordions, {
+  openOnInit: [0],
   showMultiple: true,
   onOpen: function (currentElement) {
-    console.log(currentElement);
+    let use = currentElement.querySelector('use[href]');
+    use.setAttribute('href', '../img/icons.svg#icon-arrow-up');
+  },
+  onClose: function (currentElement) {
+    let use = currentElement.querySelector('use[href]');
+    use.setAttribute('href', '../img/icons.svg#icon-arrow-down');
   },
 });
-
-// // Default options
-// new Accordion('.accordion-container');
-
-// new Accordion('.container-first');
-
-// // User options
-// new Accordion('.container-second', {
-//   duration: 400,
-//   showMultiple: true,
-//   onOpen: function (currentElement) {
-//     console.log(currentElement);
-//   },
-// });
-
-// // Define several accordions with the same options (pass an array with selectors)
-// new Accordion(['.container-first', '.container-second'], {});
-
-// // or pass an array with HTMLElements
-// const accordions = Array.from(
-//   document.querySelectorAll('.accordion-container')
-// );
-// new Accordion(accordions, {});
-
-// // Detach events
-// const accordion = new Accordion('.container-first');
-// accordion.detachEvents();
