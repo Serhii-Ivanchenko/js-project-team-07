bea561bd7aa8769bfbd19e855e8bf19191bf947e
Juliiaks <juliakharetonchuk@gmail.com>
2024-04-27 00:02:00 +0300

    add js

---
+++ src/js/header.js
@@ -0,0 +1,27 @@
+const menu = document.querySelector(".menu-title");
+const menuList = document.querySelector(".menu-list");
+const openBtn = document.querySelector(".open-mobile-menu");
+const closeBtn = document.querySelector(".close-menu");
+const mobMenu = document.querySelector(".mobile-menu");
+const mobMenuList = document.querySelector(".mobile-menu-list");
+const orderProject = document.querySelector(".order-project-mobm")
+
+menu.addEventListener("click", handleClick);
+
+function handleClick() {
+    menuList.classList.toggle("hidden");
+}
+
+openBtn.addEventListener("click", openBtnClick)
+
+function openBtnClick() {
+    mobMenu.classList.add("is-open");
+}
+
+closeBtn.addEventListener("click", closeBtnClick);
+mobMenuList.addEventListener("click", closeBtnClick);
+orderProject.addEventListener("click", closeBtnClick);
+
+function closeBtnClick() {
+    mobMenu.classList.remove("is-open");
+}
