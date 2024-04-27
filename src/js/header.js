61851cdf9350dbd326e00755115ee391fac5738b
Juliiaks <juliakharetonchuk@gmail.com>
2024-04-27 10:47:40 +0300

    Update header.js

--- src/js/header.js
+++ src/js/header.js
@@ -1,10 +1,11 @@
 const menu = document.querySelector(".menu-title");
 const menuList = document.querySelector(".menu-list");
 const openBtn = document.querySelector(".open-mobile-menu");
-const closeBtn = document.querySelector(".close-menu");
 const mobMenu = document.querySelector(".mobile-menu");
-const mobMenuList = document.querySelector(".mobile-menu-list");
-const orderProject = document.querySelector(".order-project-mobm");
+
+// const mobMenuList = document.querySelector(".mobile-menu-list");
+// const orderProject = document.querySelector(".order-project-mobm");
+// const closeBtn = document.querySelector(".close-menu");
 
 menu.addEventListener("click", handleClick);
 menuList.addEventListener("click", handleClick);
@@ -19,10 +20,13 @@ function openBtnClick() {
     mobMenu.classList.add("is-open");
 }
 
-closeBtn.addEventListener("click", closeBtnClick);
-mobMenuList.addEventListener("click", closeBtnClick);
-orderProject.addEventListener("click", closeBtnClick);
+mobMenu.addEventListener("click", closeBtnClick);
 
 function closeBtnClick() {
     mobMenu.classList.remove("is-open");
 }
+
+// closeBtn.addEventListener("click", closeBtnClick);
+// mobMenuList.addEventListener("click", closeBtnClick);
+// orderProject.addEventListener("click", closeBtnClick);
+
