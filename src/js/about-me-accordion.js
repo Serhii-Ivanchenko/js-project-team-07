f01478af51f15e6f49d5861e849ef03cbe4c94ff
vButkevich <vyacheslav.butkevich@gmail.com>
2024-04-30 15:43:34 +0300

    rename accordion classes

--- src/js/about-me-accordion.js
+++ src/js/about-me-accordion.js
@@ -1,18 +1,18 @@
 import { createAccordion, handleClick } from './accordion';
 
-const accordionList = document.querySelector('.accordion-container');
+const accordionList = document.querySelector('.aboutme-accordion-container');
 
 const options = {
-  containerClass: '.accordion-container',
-  elementClass: 'aboutme-item',
-  triggerClass: 'aboutme-ac-trigger',
-  panelClass: 'aboutme-item-panel',
+  containerClass: '.aboutme-accordion-container',
+  elementClass: 'aboutme-accordion-item',
+  triggerClass: 'aboutme-accordion-trigger',
+  panelClass: 'aboutme-accordion-panel',
   openOnInit: [0],
 };
 
 const clickOptions = {
   btnClass: '.aboutme-accordion-btn',
-  iconClass: '.aboutme-item-btn',
+  iconClass: '.aboutme-item-icon',
 };
 
 accordionList.addEventListener('click', event =>
